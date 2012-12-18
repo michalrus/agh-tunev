bb09e2ac5bedbb2e7055ff35d7201f375acd33d0
Michał Rus <m@michalrus.com>
2012-12-18 22:28:37 +0100

    Read FDS input file... more-or-less

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import java.io.FileNotFoundException;
+
 import board.Board;
 
 public class Main {
 
 	/**
+	 * Tymczasowo wyrzuca FileNotFoundException, póki nie bêdzie (o ile bêdzie!)
+	 * kiedyœ wybierania pliku wejœciowego FDS-a z poziomu UI.
+	 * 
 	 * @param args
+	 * @throws FileNotFoundException
 	 */
-	public static void main(String[] args) {
-		Board board = new Board("firedata/tunnel.fds");
+	public static void main(String[] args) throws FileNotFoundException {
+		Board board;
+
+		board = new Board("firedata/tunnel.fds");
 
 		board.initAgents(); // w jaki sposób? gdzie inicjalni agenci? random?
 
--- src/board/Board.java
+++ src/board/Board.java
@@ -1,18 +1,95 @@
 package board;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import agent.Agent;
 import agent.Neighborhood;
 
 public class Board {
 
+	private int width, length;
+
 	private ArrayList<Agent> agents;
 
-	public Board(String path) {
+	public Board(String path) throws FileNotFoundException {
 		agents = new ArrayList<Agent>();
+
+		parseFDSInputFile(path);
+	}
+
+	/**
+	 * Wczytywanie danych z pliku wejœciowego FDS-a.
+	 * 
+	 * Pamiêtaj: x to d³ugoœæ tunelu, y to szerokoœæ. Podajemy czêsto
+	 * wspó³rzêdne x,y,z albo x,x1,y,y1,z,z1.
+	 * 
+	 * @param path
+	 * @throws FileNotFoundException
+	 */
+	private void parseFDSInputFile(String path) throws FileNotFoundException {
+		boolean gotDimensions = false;
+		Pattern patternDimensions = Pattern
+				.compile("^&MESH\\s+IJK=(\\d+),(\\d+),\\d+");
+		Pattern patternObstacle = Pattern
+				.compile("^&OBST\\s+XB=(\\d+),(\\d+),(\\d+),(\\d+),\\d+,\\d+");
+		Matcher matcher;
+
+		String line;
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		try {
+			while ((line = br.readLine()) != null) {
+				// dimensions
+				if (!gotDimensions) {
+					matcher = patternDimensions.matcher(line);
+					if (matcher.find()) {
+						width = Integer.parseInt(matcher.group(1));
+						length = Integer.parseInt(matcher.group(2));
+						gotDimensions = true;
+						continue;
+					}
+				}
+
+				// obstacles
+				matcher = patternObstacle.matcher(line);
+				if (matcher.find()) {
+					int x1 = Integer.parseInt(matcher.group(1));
+					int x2 = Integer.parseInt(matcher.group(2));
+					int y1 = Integer.parseInt(matcher.group(3));
+					int y2 = Integer.parseInt(matcher.group(4));
+					addObstacle(x1, x2, y1, y2);
+					continue;
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (br != null)
+				try {
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if (!gotDimensions)
+				throw new RuntimeException(path
+						+ ": no mesh dimensions declared");
+		}
+	}
+
+	private void addObstacle(int x1, int x2, int y1, int y2) {
+		for (int x = x1; x < x2; x++)
+			for (int y = y1; y < y2; y++)
+				break;
+		System.out.println("addObstacle():  L:" + x1 + "-" + x2 + "  W:" + y1 + "-" + y2);
 	}
 
 	public void initAgents() {
@@ -23,6 +100,11 @@ public class Board {
 	 * Jedna iteracja symulacji.
 	 */
 	public void update() {
+		try {
+			Thread.sleep(1000); // na razie, póki nic tu nie ma do liczenia
+		} catch (InterruptedException e) {
+		}
+
 		setFdsData();
 
 		for (Agent agent : agents)
