9a279c976a167842ecf1a473483e74a2a93490df
Michał Rus <m@michalrus.com>
2012-12-18 23:24:19 +0100

    Add main Cell field to Neighbourhood

--- src/agent/Neighborhood.java
+++ src/agent/Neighborhood.java
@@ -1,14 +1,22 @@
 package agent;
 
+import board.Cell;
+
 public class Neighborhood {
 
 	public enum Direction {
 		TOP, TOPRIGHT, RIGHT, BOTTOMRIGHT, BOTTOM, BOTTOMLEFT, LEFT, TOPLEFT
 	}
 
+	private Cell first;
+
 	public int getTemperature() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	public Cell getFirstCell() {
+		return first;
 	}
 
 }
