b01814d9cc7c7f3d0db7d8ab358263dd5873e293
Michał Rus <m@michalrus.com>
2013-04-30 03:39:26 +0000

    Rename calculation.IntersectionCalc -> Common

--- src/edu/agh/tunev/model/Common.java
+++ src/edu/agh/tunev/model/Common.java
@@ -1,17 +1,21 @@
-package edu.agh.tunev.model.calculation;
+package edu.agh.tunev.model;
 
 import java.awt.Shape;
 import java.awt.geom.Area;
 import java.awt.geom.Rectangle2D;
 
-public abstract class IntersectionCalc {
+public final class Common {
 
-	public static Double calculateIntersection(Shape shape1, Shape shape2){
+	public static double calculateIntersection(Shape shape1, Shape shape2) {
 		Area area1 = new Area(shape1);
 		Area area2 = new Area(shape2);
 		area1.intersect(area2);
 		Rectangle2D rect = area1.getBounds2D();
-		
-		return rect.getWidth() * rect.getHeight();	
+
+		return rect.getWidth() * rect.getHeight();
 	}
+	
+	private Common() {
+	}
+
 }
