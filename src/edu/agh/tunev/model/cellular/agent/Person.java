cef2fa0ddaccfd674d7f80ed2b7e295d88e6dfe3
Kuba Rakoczy <kuba.rakoczy@gmail.com>
2013-05-03 00:34:27 +0300

    Map Cell index in Person

--- src/edu/agh/tunev/model/cellular/agent/Person.java
+++ src/edu/agh/tunev/model/cellular/agent/Person.java
@@ -7,30 +7,28 @@ import edu.agh.tunev.model.AbstractPerson;
 import edu.agh.tunev.model.cellular.grid.Cell;
 
 public final class Person extends AbstractPerson {
-	
 
-	public enum Orientation{
+	public enum Orientation {
 		E, NE, N, NW, W, SW, S, SE;
-		
-		public static int getIndexOf(Orientation orient){
+
+		public static int getIndexOf(Orientation orient) {
 			Person.Orientation[] values = Person.Orientation.values();
 			int ind;
 
-			for (ind = 0; ind < values.length  && values[ind] != orient; ++ind)
+			for (ind = 0; ind < values.length && values[ind] != orient; ++ind)
 				;
-			
+
 			return ind;
 		}
 	}
-	
+
 	private Cell cell;
-	
+
 	public Person(Cell _cell) {
 		super(Cell.d2c(_cell.getPosition()));
 		this.cell = _cell;
 	}
-	
-	
+
 	/**
 	 * Maps {@code Orientation} to corresponding angle.
 	 * 
@@ -38,33 +36,43 @@ public final class Person extends AbstractPerson {
 	 * @return
 	 * @throws WrongOrientationException
 	 */
-	public static Double orientToAngle(Orientation orient) throws WrongOrientationException{	
-		//starting at east
+	public static Double orientToAngle(Orientation orient)
+			throws WrongOrientationException {
+		// starting at east
 		Double angle = 0.0;
 		Person.Orientation[] orientValues = Person.Orientation.values();
 		int i;
-		
-		//TODO: more sensible error handling
-		for(i = 0; i < orientValues.length && orient != orientValues[i]; ++i){
+
+		for (i = 0; i < orientValues.length && orient != orientValues[i]; ++i) {
 			angle += 45.0;
 		}
-		
-		if(i > 7)
+
+		if (i > 7)
 			throw new WrongOrientationException();
-		else 
+		else
 			return angle;
 	}
 
-	
-	private Double evaluateFieldPotential(){
+	private Double evaluateFieldPotential() {
 		return null;
 	}
-	
-	private int positionToIndex(Cell c){
+
+	/**
+	 * <pre> A snippet mapping position to neighbour index required in AllowedCfgs.
+	 *	Indexes:
+	 *  0   1   2
+	 *  3       4
+	 *  5   6   7
+	 * 
+	 * @param c
+	 * @return
+	 */
+	private int positionToIndex(Cell c) {
 		Point posOth = c.getPosition();
-		Point posCell = cell.getPosition();
+		Point posCell = this.cell.getPosition();
 		
-		//TODO: finish
-		return 0;
+		return (posOth.x - posCell.x + 2) + 3
+				* Math.abs(posOth.y - posCell.y - 2);
 	}
+
 }
