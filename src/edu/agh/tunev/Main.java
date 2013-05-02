9d289f027f8744f85cdcb45b9d7a6673fe571da1
Michał Rus <m@michalrus.com>
2013-05-03 01:40:17 +0200

    Revert Main to <2013-05-01 01:01:11>

--- src/edu/agh/tunev/Main.java
+++ src/edu/agh/tunev/Main.java
@@ -1,25 +1,17 @@
 package edu.agh.tunev;
 
-import java.awt.Point;
-import java.awt.geom.Point2D;
-import java.util.Vector;
-
-import edu.agh.tunev.model.cellular.agent.WrongOrientationException;
-import edu.agh.tunev.model.cellular.grid.Cell;
+import edu.agh.tunev.ui.MainFrame;
 
 public class Main {
 
 	/**
-	 * Tutaj rejestrujemy wszystkie nasze modele, które mają być widoczne w
-	 * UI.
+	 * Tutaj rejestrujemy wszystkie nasze modele, które mają być widoczne w UI.
 	 * 
 	 * @param args
-	 * @throws WrongOrientationException 
 	 */
-	public static void main(String[] args) throws WrongOrientationException {
-		// MainFrame.register(edu.agh.tunev.model.cellular.Model.class);
+	public static void main(String[] args) {
+		MainFrame.register(edu.agh.tunev.model.cellular.Model.class);
 
-		// new MainFrame();
-		
+		new MainFrame();
 	}
 }
