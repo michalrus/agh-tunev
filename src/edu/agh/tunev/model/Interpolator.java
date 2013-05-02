3728ef09c63cca6cd9bd710b40df7d592d938300
Michał Rus <m@michalrus.com>
2013-05-02 16:22:53 +0200

    Add these properties to PersonState

--- src/edu/agh/tunev/model/Interpolator.java
+++ src/edu/agh/tunev/model/Interpolator.java
@@ -11,13 +11,19 @@ public final class Interpolator {
 
 	public static class PersonState {
 		public final Point2D.Double position;
+		public final double orientation;
+		public final AbstractPerson.Movement movement;
 	
 		public PersonState(AbstractPerson person) {
 			position = person.getPosition();
+			orientation = person.getOrientation();
+			movement = person.getMovement();
 		}
 		
-		public PersonState(Point2D.Double position) {
+		public PersonState(Point2D.Double position, double orientation, AbstractPerson.Movement movement) {
 			this.position = position;
+			this.orientation = orientation;
+			this.movement = movement;
 		}
 	}
 
@@ -42,24 +48,30 @@ public final class Interpolator {
 		if (states == null)
 			return null;
 
-		final Entry<Double, Interpolator.PersonState> prev = states.floorEntry(t);
-		final Entry<Double, Interpolator.PersonState> next = states.ceilingEntry(t);
+		final Entry<Double, Interpolator.PersonState> prevEntry = states.floorEntry(t);
+		final Entry<Double, Interpolator.PersonState> nextEntry = states.ceilingEntry(t);
 		
-		if (prev == null && next == null)
+		if (prevEntry == null && nextEntry == null)
 			return null;
-		else if (prev == null)
-			return next.getValue();
-		else if (next == null)
-			return prev.getValue();
-		else if (prev == next)
-			return prev.getValue();
+		else if (prevEntry == null)
+			return nextEntry.getValue();
+		else if (nextEntry == null)
+			return prevEntry.getValue();
+		else if (prevEntry == nextEntry)
+			return prevEntry.getValue();
+
+		final PersonState prev = prevEntry.getValue();
+		final PersonState next = nextEntry.getValue();
+		
+		final double prevT = prevEntry.getKey();
+		final double nextT = nextEntry.getKey();
 		
 		// splajny 1-go stopnia? będzie git
 		
-		final double ratio = (t - prev.getKey()) / (next.getKey() - prev.getKey());
+		final double ratio = (t - prevEntry.getKey()) / (nextEntry.getKey() - prevEntry.getKey());
 
-		final Point2D.Double p = prev.getValue().position;
-		final Point2D.Double n = next.getValue().position;
+		final Point2D.Double p = prev.position;
+		final Point2D.Double n = next.position;
 
 		final double px = p.getX();
 		final double py = p.getY();
@@ -69,7 +81,7 @@ public final class Interpolator {
 		final double x = px + ratio * (nx - px);
 		final double y = py + ratio * (ny - py);
 		
-		return new Interpolator.PersonState(new Point2D.Double(x, y));
+		return new Interpolator.PersonState(new Point2D.Double(x, y), prev.orientation, prev.movement);
 	}
 
 	public Interpolator() {
